1352ba58ad1ae3b21d6f87063932b72f362230b4
Artem Sokolov <asokolov@accesssoftek.com>
2017-10-08 15:49:29 +0500

    Added: Load telegram bot settings

--- Logic/TelegramBot.cs
+++ Logic/TelegramBot.cs
@@ -1,11 +1,30 @@
 ﻿using System.Net;
+using System.Linq;
 using System.Net.Http;
+using System.Xml;
+using System.Xml.Linq;
 //using Telegram.Bot.Types;
 
 namespace Uchilka.Logic
 {
     internal class TelegramBot
     {
+        private XmlNodeList userNodes;
+
+        public TelegramBot()
+        {
+            var settingsFile = new XmlDocument();
+            settingsFile.Load("secrets.xml");
+
+            var token = settingsFile.DocumentElement.ChildNodes[0].ChildNodes[0].InnerText;
+            userNodes = settingsFile.DocumentElement.ChildNodes[0].ChildNodes[1].ChildNodes;
+
+            foreach(XmlElement node in users)
+            {
+                
+            }
+
+        }
         public bool TestApiAsync()
         {
             var handler = new HttpClientHandler
